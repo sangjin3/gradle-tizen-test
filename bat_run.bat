75553c353affc6d4dce81375e77327dee8d510d5
dkyos <dkyun77@gmail.com>
2015-12-04 00:49:35 +0900

    update ReinstallNativeTask => ReNativeTask
    Signed-off-by: dkyos <dkyun77@gmail.com>

--- bat_run.bat
+++ bat_run.bat
@@ -1,16 +1,16 @@
 @echo on
 
-START /wait gradlew.bat -q vmTest
+call gradlew.bat -q vmTest
 
-START /wait gradlew.bat -q certTest
+call gradlew.bat -q certTest
 
-START /wait gradlew.bat -q nativeTest
-START /wait gradlew.bat -q installNativeTest
-START /wait gradlew.bat -q reinstallNativeTest
+call gradlew.bat -q nativeTest
+call gradlew.bat -q installNativeTest
+call gradlew.bat -q reinstallNativeTest
 
-START /wait gradlew.bat -q webTest
-START /wait gradlew.bat -q installWebTest
+call gradlew.bat -q webTest
+call gradlew.bat -q installWebTest
 
-START /wait gradlew.bat -q sdbTest
+call gradlew.bat -q sdbTest
 
 
