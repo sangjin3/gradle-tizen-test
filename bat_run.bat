eb5f0f2f773056af133db443e9014c738f02af75
dkyos <dkyun77@gmail.com>
2015-12-04 10:26:02 +0900

    update windows test script

--- bat_run.bat
+++ bat_run.bat
@@ -1,16 +1,22 @@
 @echo on
 
-call gradlew.bat -q vmTest
+set var=mobile-2.3 wearable-2.3 wearable-2.3.2
 
-call gradlew.bat -q certTest
+for %%i in (%var%) do (
 
-call gradlew.bat -q nativeTest
-call gradlew.bat -q installNativeTest
-call gradlew.bat -q reNativeTest
+    call gradlew.bat -q -Pplatform=%%i vmTest
+    call gradlew.bat -q -Pplatform=%%i certTest
 
-call gradlew.bat -q webTest
-call gradlew.bat -q installWebTest
+    call gradlew.bat -q -Pplatform=%%i nativeTest
+    call gradlew.bat -q -Pplatform=%%i installNativeTest
+    call gradlew.bat -q -Pplatform=%%i reNativeTest
 
-call gradlew.bat -q sdbTest
+    call gradlew.bat -q -Pplatform=%%i webTest
+    call gradlew.bat -q -Pplatform=%%i installWebTest
 
+    call gradlew.bat -q -Pplatform=%%i sdbTest
 
+    taskkill /f /im "emulator-x86.exe"
+    taskkill /f /im "emulator-x86.exe"
+    timeout /t 10
+)
