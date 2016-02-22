5f7522eca42356b2b6e1b7218cff3fbba8fdfcc1
dkyos <dkyun77@gmail.com>
2016-02-22 15:36:43 +0900

    update
    Signed-off-by: dkyos <dkyun77@gmail.com>

--- bat_run.bat
+++ bat_run.bat
@@ -1,20 +1,21 @@
 @echo on
 
-set var=mobile-2.3 wearable-2.3 wearable-2.3.2
+set sdk_path="C:/tizen-sdk_24_rev4"
+set var=mobile-2.4 mobile-2.3.1 mobile-2.3 wearable-2.3.1
 
 for %%i in (%var%) do (
 
-    call gradlew.bat -q -Pplatform=%%i vmTest
-    call gradlew.bat -q -Pplatform=%%i certTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i vmTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i certTest
 
-    call gradlew.bat -q -Pplatform=%%i nativeTest
-    call gradlew.bat -q -Pplatform=%%i installNativeTest
-    call gradlew.bat -q -Pplatform=%%i reNativeTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i nativeTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i installNativeTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i reNativeTest
 
-    call gradlew.bat -q -Pplatform=%%i webTest
-    call gradlew.bat -q -Pplatform=%%i installWebTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i webTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i installWebTest
 
-    call gradlew.bat -q -Pplatform=%%i sdbTest
+    call gradlew.bat -q -Psdk_path=%sdk_path% -Pplatform=%%i sdbTest
 
     taskkill /f /im "emulator-x86.exe"
     taskkill /f /im "emulator-x86.exe"
